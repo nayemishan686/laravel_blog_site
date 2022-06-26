d68f3e671b799bd9e80800a2bb0e1b8c75df203f
Nayem Ishan <nayemishan377@gmail.com>
2022-06-26 15:56:51 +0600

    Registration form templating

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -65,7 +65,7 @@
                         <a href="{{ route('password.request') }}">I forgot my password</a>
                     </p>
                     <p class="mb-0">
-                        <a href="register.html" class="text-center">Register a new membership</a>
+                        <a href="{{route('register')}}" class="text-center">Register a new membership</a>
                     </p>
                 </div>
                 <!-- /.login-card-body -->
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,7 +1,91 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+    <div class="hold-transition register-page">
+        <div class="register-box">
+            <div class="register-logo">
+                <a href="{{url('/')}}"><b>Nayems</b>BLOG</a>
+            </div>
+
+            <div class="card">
+                <div class="card-body register-card-body">
+                    <p class="login-box-msg">Register a new membership</p>
+
+                    <form method="POST" action="{{ route('register') }}">
+                        @csrf
+                        <div class="input-group mb-3">
+                            <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" autocomplete="name" autofocus>
+                            <div class="input-group-append">
+                                <div class="input-group-text">
+                                    <span class="fas fa-user"></span>
+                                </div>
+                            </div>
+                            @error('name')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
+                        </div>
+                        <div class="input-group mb-3">
+                            <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" autocomplete="email">
+                            <div class="input-group-append">
+                                <div class="input-group-text">
+                                    <span class="fas fa-envelope"></span>
+                                </div>
+                            </div>
+                            @error('email')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
+                        </div>
+                        <div class="input-group mb-3">
+                            <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" autocomplete="new-password">
+                            <div class="input-group-append">
+                                <div class="input-group-text">
+                                    <span class="fas fa-lock"></span>
+                                </div>
+                            </div>
+                            @error('password')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                        </div>
+                        <div class="input-group mb-3">
+                            <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                                <div class="input-group-text">
+                                    <span class="fas fa-lock"></span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row px-3">
+                            <div class="col-8">
+                                <div class="icheck-primary">
+                                    <input type="checkbox" id="agreeTerms" name="terms" value="agree">
+                                    <label for="agreeTerms">
+                                        I agree to the <a href="#">terms</a>
+                                    </label>
+                                </div>
+                            </div>
+                            <!-- /.col -->
+                            <div class="col-4">
+                                <button type="submit" class="btn btn-primary btn-block">Register</button>
+                            </div>
+                            <!-- /.col -->
+                        </div>
+                    </form>
+
+                    <a href="{{route('login')}}" class="px-4 pb-3">I already have a membership</a>
+                </div>
+                <!-- /.form-box -->
+            </div><!-- /.card -->
+        </div>
+    </div>
+@endsection
+
+
+{{-- <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
@@ -73,5 +157,4 @@
             </div>
         </div>
     </div>
-</div>
-@endsection
+</div> --}}
